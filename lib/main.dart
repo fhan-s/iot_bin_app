1e47ffe5babf487493a9ef5c9bde26eaa44f1116
fhan-s <farhansalik2@gmail.com>
2026-01-15 01:29:29 +0000

    integrated supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://ikbjcupnufrhgrwalqjz.supabase.co',
+    anonKey: 'sb_publishable_veDGu6wFXYjZs-AvVLiT3A__sYuNTOt',
+  );
+  runApp(MyApp());
 }
+
+final supabase = Supabase.instance.client;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -29,11 +37,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  List<String> bins = [];
+  Future<void> _getAllBins() async {
+    final bin_test_data = await supabase.from('test_bins').select('bin_name');
     setState(() {
-      _counter++;
+      bins = bin_test_data.map<String>((row) => row['bin_name']).toList();
     });
   }
 
@@ -44,22 +52,16 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: _getAllBins,
+            child: const Text('Get all bins'),
+          ),
+          const SizedBox(height: 20),
+          for (final bin in bins) Text(bin),
+        ],
       ),
     );
   }
