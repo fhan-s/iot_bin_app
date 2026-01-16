1ad2e4934328460b675a76f435115bcc7608f389
fhan-s <farhansalik2@gmail.com>
2026-01-16 17:47:30 +0000

    properly configured 2 logins using triggers

--- lib/auth/auth_gate.dart
+++ lib/auth/auth_gate.dart
@@ -16,14 +16,15 @@ class AuthGate extends StatelessWidget {
         .from('janitorial_staff')
         .select('role')
         .eq('id', user.id)
-        .single();
-    return response['role'] as String?;
+        .maybeSingle();
+    return response?['role'] as String?;
   }
 
   @override
   Widget build(BuildContext context) {
+    final supabase = Supabase.instance.client;
     return StreamBuilder(
-      stream: Supabase.instance.client.auth.onAuthStateChange,
+      stream: supabase.auth.onAuthStateChange,
       builder: (context, snapshot) {
         try {
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -31,7 +32,7 @@ class AuthGate extends StatelessWidget {
               body: Center(child: CircularProgressIndicator()),
             );
           }
-          final session = Supabase.instance.client.auth.currentSession;
+          final session = supabase.auth.currentSession;
           // User is already logged in
           if (session != null) {
             return FutureBuilder<String?>(
@@ -45,11 +46,14 @@ class AuthGate extends StatelessWidget {
                 final role = roleSnapshot.data;
                 // Navigate based on user role
                 if (role == "manager") {
+                  debugPrint("<LOGIN OK---------: $role");
                   return const ManagerDashboardPage();
                 } else if (role == "janitor") {
+                  debugPrint("<LOGIN OK---------: $role");
                   return const JanitorDashboardPage();
                 } else {
                   // Unknown role or no role assigned
+                  debugPrint("LOGIN FAILED------: $role");
                   return const LoginPage();
                 }
               },
@@ -59,6 +63,7 @@ class AuthGate extends StatelessWidget {
             return const LoginPage();
           }
         } catch (e) {
+          debugPrint("LOGIN FAILED-------: $e");
           // In case of any error, redirect to login page
           return const LoginPage();
         }
--- lib/pages/janitor/dashboard.dart
+++ lib/pages/janitor/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iot_bin_app/main.dart';
 
 class JanitorDashboardPage extends StatefulWidget {
   const JanitorDashboardPage({super.key});
@@ -8,10 +9,25 @@ class JanitorDashboardPage extends StatefulWidget {
 }
 
 class _JanitorDashboardPageState extends State<JanitorDashboardPage> {
+  Future<void> logout() async {
+    await supabase.auth.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Janitor Dashboard')),
+      appBar: AppBar(
+        title: const Text('Janitor Dashboard'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            tooltip: "Logout",
+            onPressed: () {
+              logout();
+            },
+          ),
+        ],
+      ),
       body: const Center(child: Text('Welcome to the Janitor Dashboard!')),
     );
   }
